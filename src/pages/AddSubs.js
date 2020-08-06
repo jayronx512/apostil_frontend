330b60711d6d94ed63cb2e4de59706261a30ffc7
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-08-06 21:30:47 +0800

    adjust input amount

--- src/pages/AddSubs.js
+++ src/pages/AddSubs.js
@@ -50,7 +50,7 @@ function AddSubs() {
 
     const moneyInputStyle={
         width: '75vw',
-        height: '20vh',
+        height: '60px',
         textAlign: 'center',
         fontSize: '10vw',
         fontFamily: 'Crete Round, serif'
